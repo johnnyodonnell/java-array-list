629f46dd33521b35e9d4b59805ad674731a5b88d
Johnny O'Donnell <odjohnny@amazon.com>
2022-04-03 16:52:03 -0700

    Update info metadata

--- info.rkt
+++ info.rkt
@@ -1,9 +1,8 @@
 #lang info
 (define collection "java-array-list")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/java-array-list.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "Clone of Java's ArrayList")
 (define version "0.0")
 (define pkg-authors '(johnny))
 (define license '(Apache-2.0 OR MIT))
+
